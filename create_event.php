d7d0fa1b02c98114d91c3473ad5c218970a792f7
Logan McCamon <logan@mccamon.org>
2013-03-28 13:02:39 -0500

    update
    known bug in PDO insert shift statement
    throws uncaught exception

---
+++ create_event.php
@@ -0,0 +1,190 @@
+<?
+
+function newPDO(){
+	$dsn = "mysql:host=localhost;dbname=CS430";
+	$user = "root";
+	$pass = "root";
+	return new PDO($dsn, $user, $pass);
+}
+
+function validateForm(){
+
+
+// retrieve all post values
+$eventName = $_POST['eventName'];
+$eventDescription = $_POST['eventDescription'];
+$eventDate = $_POST['eventDate'];
+$type = $_POST['type'];
+$type = 3;
+$fundraising = $_POST['fundraising'];
+$eventLocation = $_POST['eventLocation'];
+$publicNotes = $_POST['publicNotes'];
+$privateNotes = $_POST['privateNotes'];
+$recurring = $_POST['recurring'];
+$DOW = $_POST['DOW'];
+$startTime = $_POST['startTime'];
+$endTime = $_POST['endTime'];
+echo "<br/>.".$startTime.".<br/>.".$endTime.".<br/>";
+$max = $_POST['max'];
+$projectLeader = $_POST['projectLeader'];
+
+// check to see if required items have values
+if($eventName == NULL || $eventDescription == NULL || $eventDate == NULL || $eventLocation == NULL || $projectLeader == NULL || $max == NULL)
+	{
+	  echo '<div class="entry"><strong>All of the required fields were not filled out.  Please try again.</strong></div>';
+	 // print_form();
+	}
+
+// if the event is recurring, additional information is required
+if($recurring != NULL && ($DOW == NULL || $startTime == NULL || $endTime == NULL)){
+	echo '<div class="entry"><strong>All of the required fields were not filled out.  Please try again.</strong></div>';
+	// print_form();
+}
+// next steps are to range check some of the values and then insert them into the appropriate tables
+
+
+   	$db = newPDO();
+   	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+   	
+   	$sql = "INSERT INTO Event (Name,DOW,Description,Type,Location,publicNotes,privateNotes)
+   			VALUES (:eventName,:DOW,:eventDescription,:type,:eventLocation,:publicNotes,:privateNotes)";
+
+	$stmt = $db->prepare($sql);
+
+	$stmt->execute(array(':eventName'=>$eventName,
+						 ':DOW'=>$DOW,
+						 ':eventDescription'=>$eventDescription,
+						 ':type'=>$type,
+						 ':eventLocation'=>$eventLocation,
+						 ':publicNotes'=>$publicNotes,
+						 ':privateNotes'=>$privateNotes));
+
+	print_r($stmt->errorInfo());
+	$affected_rows = $stmt->rowCount();
+	echo $affected_rows;
+	
+	$E_Id_LastInsert = $db->lastInsertId();
+	echo $E_Id_LastInsert;
+
+	/* uncaught exception thrown. reason unknown */
+	$sql = "INSERT INTO Shift (E_Id,startTime,endTime,Max) 
+			VALUES (:E_Id,:startTime,:endTime,:Max)";
+
+	$stmt = $db->prepare($sql);
+	echo "<br/>".$max."<br/>";
+	$stmt->execute(array(':E_Id'=>$E_Id_LastInsert,
+						 ':startTime'=>$startTime,
+						 ':endTime'=>$endTime,
+						 ':max'=>$max));
+
+	print_r($stmt->errorInfo());
+}
+
+function createEvent() {   
+echo <<<END
+	<div class="content">
+	<form method="POST"> 
+<p> 
+<b>The "Create Event" Form</b><br/> 
+<label for="eventName">Event Name</label> 
+<input type="text" name="eventName" value="Sample Event Name"/><br/>
+
+<label for="eventDescription">Event Description</label><br/>
+<textarea rows="3" cols="20" name="eventDescription">Event Description goes here</textarea><br/>
+
+<label for="eventDate">Event Date</label>
+<input type="date" name="eventDate"/><br/>
+
+<label for="type">Event Type</label>
+<select name="type">
+	<option value="" selected>Community</option>
+	<option value="">Chapter</option>
+	<option value="">Campus</option>
+	<option value="">Country</option>
+	<option value="">Broho</option>
+</select><br/>
+
+<label for="fundraising">Fundraising?</label>
+<input type="radio" name="fundraising" value="T">yes
+<input type="radio" name="fundraising" value="F" checked>no<br/>
+
+<label for="Location">Location</label>
+<input type="text" name="eventLocation" value="SUB"/><br/>
+
+<label for="Notes">Public Notes</label><br/>
+<textarea rows="3" cols="20" name="publicNotes">public note test</textarea><br/>
+
+<label for="Notes">Private Notes</label><br/>
+<textarea rows="3" cols="20" name="privateNotes">private note test</textarea><br/>
+
+<label for="Recurring">Recurring?</label><br/>
+<input type="radio" name="recurring" value="T">yes
+<input type="radio" name="recurring" value="F" checked>no<br/>
+
+<label for="type">Event DOW</label>
+<select name="DOW">
+	<option value="Sun" selected>Sunday</option>
+	<option value="Mon">Monday</option>
+	<option value="Tue">Tuesday</option>
+	<option value="Wed">Wednesday</option>
+	<option value="Thu">Thursday</option>
+	<option value="Fri">Friday</option>
+	<option value="Sat">Saturday</option>
+</select><br/>
+
+<!-- 
+---- the following three should be for each shift 
+---- also, shifts are any time slot for an event
+---- on a given day 
+ -->
+
+<label for="eventStartTime">Start Time: </label>
+<input type="time" name="startTime"/><br/>
+
+<label for="eventEndTime">End Time: </label>
+<input type="time" name="endTime"/><br/>
+
+<label for="maxEnrollment">Max Enrollment</label>
+<input type="number" name="max" min="1" max="150" value="10"/><br/>
+
+<label for="projectLeader">Project Leader</label>
+<select name="projectLeader">
+	<option value="theirName">Roster</option>
+</select>
+<input type="hidden" name="stage" value="verify" />
+ 	<p align="left"><input type="submit" value="Submit" /></p>
+</form> 
+END;
+
+echo "<hr/>";
+} 
+
+
+if(isset($_POST['stage'])){
+	validateForm();
+}else{
+createEvent();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
